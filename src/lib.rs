325e02ca5b261ce05f30b6b49e8a07d8a08f6c66
Håvar Nøvik <havar@ravn.no>
2016-03-25 11:49:43 +0100

    Updated license stuff.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+// Copyright (c) 2016 multimap developers
+//
+// Licensed under the Apache License, Version 2.0
+// <LICENSE-APACHE or http://www.apache.org/licenses/LICENSE-2.0> or the MIT
+// license <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. All files in the project carrying such notice may not be copied,
+// modified, or distributed except according to those terms.
+
 //! A MultiMap implementation which is just a wrapper around std::collections::HashMap.
 //! See HashMap's documentation for more details.
 //!
